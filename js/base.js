176e26991df7925e2b5b1d9a1891a3580b016bd0
jakecrump <crumps589@gmail.com>
2019-02-27 15:46:02 -0500

    first push

---
+++ js/base.js
@@ -0,0 +1,20 @@
+
+
+
+
+itemWatch = [];
+
+var $work = $('.work-item');
+
+$work.each(function(i,obj){
+
+	itemWatch[i] = scrollMonitor.create($work.eq(i), {top:-200, bottom:-200});
+
+	itemWatch[i].enterViewport(function(){
+		$work.eq(i).addClass('show');
+		console.info(i + 'is shown')
+	});
+	itemWatch[i].exitViewport(function(){
+		$work.eq(i).removeClass('show');
+	});
+});
